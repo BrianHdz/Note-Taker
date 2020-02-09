2e8aa42d668adc1c3d602e39602c19fa251691b1
BrianHdz <popcorn46290@sbcglobal.net>
2020-02-08 23:11:17 -0800

    The save button will add the note to the sidebar & correctly write to db.json. Need to store dependencies into a hidden folder.

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const db = require("./db/db.json");
 const uuidv4 = require('uuid/v4');
+
 // const axios = require("axios");
 // // const $ = require("jquery");
 // var jsdom = require('jsdom');
@@ -44,28 +45,82 @@ app.get("/notes", function (req, res) {
 });
 
 app.get("/api/notes", function (req, res) {
-   res.json(db); 
+    res.json(db);
 });
 
 // Displays all notes in sidebar
 app.post("/api/notes", function (req, res) {
+
+    // This gives the new note a unique ID
     id = uuidv4();
     req.body.id = id;
+
+    // This pushes the new note to the sidebar
     db.push(req.body);
     res.json(true);
-    console.log(req.body);
+    // console.log(req.body);
+
+    // The next few steps will push the new note into the db.json file
+    // This will read the db.json file and turn it into var database.
+    let database = fs.readFileSync("./db/db.json", "utf-8");
+
+    //this turns the JSON string into a JS array
+    let array = JSON.parse(database);
+    let note = (req.body);
+    //console.log(array);
+    //console.log(note);
+
+    // appending "note" object to the JS "array"
+    array.push(note);
+    //console.log(array);
+
+    // Stringify "array" so we can write/send it back to db.json
+    array = JSON.stringify(array);
+    console.log(array);
+
+    // write'ing back to db.json
+    fs.writeFileSync("./db/db.json",array,"utf-8");
+
 });
 
 
 
-//     var fs = require("fs");
 
-// fs.appendFile("log.txt", process.argv[2], function(err) {
+// fs.appendFile("./db/db.json", req.body, function (err) {
 
-//   if (err) {
-//     return console.log(err);
-//   }
+//     if (err) {
+//         return console.log(err);
+//     }
 
-//   console.log("Success!");
+//     console.log("Success!");
 
 // });
+// ----------------------------------------------------
+//this turns the JSON string into a JS array
+// let array = JSON.parse(database);
+// let note = (req.body);
+// console.log(array);
+// console.log(note);
+
+// // appending my object to the JS newArray
+// // let newArray = 
+// array.push(note);
+// console.log(array);
+
+//--------------------------------------------------------
+// This will read the db.json file and turn it into var database.
+// let database = fs.readFileSync("./db/db.json", "utf-8");
+// console.log(database);
+
+// //Then we take the new note and turn that into variable "note"
+// let note = (req.body);
+// console.log(note);
+
+// // Push "note" into "database"
+// database.push();
+// console.log(newData);
+
+// // Turn the JSON string into a JS array
+// let array = JSON.parse(newData);
+
+// console.log(array);
